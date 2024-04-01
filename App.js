45e2247b1848fec627283e7d61f85c2c8a126afc
Anhar <anhar_tasman@yahoo.com>
2024-04-01 12:17:44 +0700

    inputContainer bottom padding

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal!' ></TextInput>
-        <Button title='Add Goal'></Button>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+        ></TextInput>
+        <Button title="Add Goal"></Button>
       </View>
       <View>
         <Text>List Of Goals...</Text>
@@ -17,17 +20,22 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding:50,
-  }, 
-  inputContainer:{
-    flexDirection:'row',
-    justifyContent: 'space-between',
+    paddingTop: 50,
+    paddingHorizontal: 16, // add horizontal padding
   },
-  textInput:{
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center", // make button not stretched to fit the height
+    paddingBottom: 24, // create border with bottom padding
+    borderBottomWidth: 1,
+    borderBottomColor: "#CCCCCC",
+  },
+  textInput: {
     borderWidth: 1,
-    borderColor: '#CCCCCC',
-    width: '80%',
+    borderColor: "#CCCCCC",
+    width: "70%", // decrease text input width
     marginRight: 8,
     padding: 8,
-  }
+  },
 });
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 to run
 
-npx expo start
+npm run android
