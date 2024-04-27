b238f18980733aebfe49376332477edeb7bc531e
Anhar <anhar_tasman@yahoo.com>
2024-04-27 21:15:10 +0700

    Modal background image

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,4 +1,11 @@
-import { StyleSheet, View, TextInput, Button, Modal } from "react-native";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  Modal,
+  Image,
+} from "react-native";
 import { useState } from "react";
 function GoalInput(props) {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -12,6 +19,10 @@ function GoalInput(props) {
   return (
     <Modal visible={props.visible} animationType="slide">
       <View style={styles.inputContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/goal.png")}
+        />
         <TextInput
           style={styles.textInput}
           placeholder="Your course goal!"
@@ -38,10 +49,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center", // make button not stretched to fit the height
-    marginBottom: 24, // rename to margin bottom
     padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: "#CCCCCC",
+    backgroundColor: "#311b6b",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
   },
   textInput: {
     borderWidth: 1,
